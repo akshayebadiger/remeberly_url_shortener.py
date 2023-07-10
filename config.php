3450b5ae6a31ef590744d912428dcad579d6aac0
akshayebadiger <113700275+akshayebadiger@users.noreply.github.com>
2023-07-11 01:50:37 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define( 'YOURLS_DB_NAME', 'mydatabase' );
+define( 'YOURLS_DB_USER', 'myusername' );
+define( 'YOURLS_DB_PASS', 'mypassword' );
+define( 'YOURLS_DB_HOST', 'localhost' );
+define( 'YOURLS_DB_TABLE', 'yourls_' );
