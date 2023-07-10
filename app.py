5dce5ab00242ca09a0bf498b92046db803133a59
akshayebadiger <113700275+akshayebadiger@users.noreply.github.com>
2023-07-11 00:52:50 +0530

    Update app.py
    introcuing dynamic suggestions

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ st.title('Rememberly')
 url_shortener = URLShortener()
 
 url_input = st.text_input('Enter your URL here:')
-theme = st.selectbox('Choose a theme:', ['marvel', 'hollywood', 'elements', 'animals', 'fruits'])
 if url_input:
-    short_url = url_shortener.shorten_url(url_input, theme)
+    short_url = url_shortener.shorten_url(url_input)
     st.write(f'Your shortened URL is: {short_url}')
