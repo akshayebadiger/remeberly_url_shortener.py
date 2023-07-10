1302825dd83f10cc3fabf81d4402c9561096e5ab
akshayebadiger <113700275+akshayebadiger@users.noreply.github.com>
2023-07-11 00:48:42 +0530

    Update url_shortener.py
    introucing themes

--- url_shortener.py
+++ url_shortener.py
@@ -3,24 +3,25 @@ import random
 class URLShortener:
     def __init__(self):
         self.url_mapping = {}
-        self.words = [
-            "key", "lime", "pie", "fun", "joy", "run", "jump", "play", "sky", "star", 
-            "moon", "sun", "light", "dark", "rain", "cloud", "tree", "leaf", "bird", "song",
-            "heart", "love", "laugh", "smile", "dance", "music", "book", "dream", "cake", "sweet",
-            "ice", "snow", "wind", "fire", "water", "mountain", "river", "ocean", "flower", "grass",
-            "cat", "dog", "mouse", "cheese", "fruit", "cherry", "berry", "apple", "orange", "banana"
-        ]
+        self.themes = {
+            "marvel": ["ironman", "thor", "hulk", "blackwidow", "captainamerica"],
+            "hollywood": ["star", "cinema", "director", "script", "scene"],
+            "elements": ["hydrogen", "helium", "lithium", "beryllium", "boron"],
+            "animals": ["lion", "tiger", "elephant", "giraffe", "panda"],
+            "fruits": ["apple", "banana", "cherry", "date", "elderberry"]
+        }
 
-    def generate_slug(self):
-        word1 = random.choice(self.words)
-        word2 = random.choice(self.words)
-        word3 = random.choice(self.words)
+    def generate_slug(self, theme):
+        words = self.themes.get(theme, ["key", "lime", "pie"])
+        word1 = random.choice(words)
+        word2 = random.choice(words)
+        word3 = random.choice(words)
         return f"{word1}-{word2}-{word3}"
 
-    def shorten_url(self, long_url):
+    def shorten_url(self, long_url, theme=None):
         # Ensure we generate a unique slug
         while True:
-            slug = self.generate_slug()
+            slug = self.generate_slug(theme)
             if slug not in self.url_mapping:
                 break
         self.url_mapping[slug] = long_url
