72fc2e11a7e1c1eb520d14f74d7fd404ab4fa252
akshayebadiger <113700275+akshayebadiger@users.noreply.github.com>
2023-07-11 00:59:02 +0530

    Update app.py
    css and styling

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 from url_shortener import URLShortener
 
-st.title('Remember.ly')
+st.set_page_config(page_title='Remember.ly', page_icon=":link:")
 
 url_shortener = URLShortener()
+
+st.title('Remember.ly')
+st.markdown('## Your personal and memorable URL shortener')
 
 url_input = st.text_input('Enter your URL here:')
 if url_input:
     short_url = url_shortener.shorten_url(url_input)
-    st.write(f'Your shortened URL is: {short_url}')
+    st.success(f'Your shortened URL is: {short_url}')
