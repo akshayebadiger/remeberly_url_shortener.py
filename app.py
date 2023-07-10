772137fa4fce9f66968f0ebde1687412e84c1e15
akshayebadiger <113700275+akshayebadiger@users.noreply.github.com>
2023-07-11 00:49:06 +0530

    Update app.py
    introducing themes

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from url_shortener import URLShortener
 
-st.title('URL Shortener')
+st.title('Rememberly')
 
 url_shortener = URLShortener()
 
 url_input = st.text_input('Enter your URL here:')
+theme = st.selectbox('Choose a theme:', ['marvel', 'hollywood', 'elements', 'animals', 'fruits'])
 if url_input:
-    short_url = url_shortener.shorten_url(url_input)
+    short_url = url_shortener.shorten_url(url_input, theme)
     st.write(f'Your shortened URL is: {short_url}')
